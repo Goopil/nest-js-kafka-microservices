982b09ebbb8c855e54f4ee15b83f5b1073c6d70f
zacharyvolpi <zachary.volpi@infomaniak.com>
2024-07-06 20:39:48 +0200

    improve startup

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import {config} from "@cfg/config";
 import {AppModule} from "@app/app.module";
 import {appStartup} from "./startup";
 
-async function bootstrap() {
+(async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         bufferLogs: true,
     });
+
     appStartup(app);
 
-    await app.startAllMicroservices();
-    await app.listen(config.get('app.port'));
-};
-
-void bootstrap();
+    await Promise.all([
+        app.startAllMicroservices(),
+        app.listen(config.get('app.port')),
+    ]);
+})();
